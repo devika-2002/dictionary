9d978dba4b0cc3a97aefed92b03193f729b5b74e
devikasin <devika21@navgurukul.org>
2022-08-23 22:58:31 +0530

    add one more filr

---
+++ Logical_question/Last wat count.py
@@ -0,0 +1,28 @@
+# s= "coding is fun"
+# length = 0
+
+# new_str = s.strip()
+
+# for i in range(len(new_str)):
+#     if new_str[i] == " ":
+#         length = 0
+#     else:
+#         length +=1
+# print(length)
+
+# def string():
+#     s= "coding is fun"
+    
+#     length = 0
+    
+#     new_str = s.strip()
+    
+#     for i in range(len(new_str)):
+#         if new_str[i] == " ":
+#             length = 0
+#         else:
+#             length +=1
+#     print(length)
+
+# string()
+
